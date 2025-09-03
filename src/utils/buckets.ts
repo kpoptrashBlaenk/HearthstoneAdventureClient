283020e3beb4b8f3e8f85f594d050f9a4dae16ad
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-09-03 10:10:38 +0200

    chore: format and change version

--- src/utils/buckets.ts
+++ src/utils/buckets.ts
@@ -453,6 +453,12 @@ export const BUCKETS = {
     },
     ALL_MINIONS: { params: [{ key: 'text', value: ['all', 'minions'] }], classes: null },
     RANDOM: { params: [{ key: 'text', value: ['all', 'random'] }], classes: null },
-    MASK: { params: [{ key: 'name', value: 'mask' }, { key: 'text', value: 'mask' }], classes: null },
+    MASK: {
+      params: [
+        { key: 'name', value: 'mask' },
+        { key: 'text', value: 'mask' },
+      ],
+      classes: null,
+    },
   },
 }
